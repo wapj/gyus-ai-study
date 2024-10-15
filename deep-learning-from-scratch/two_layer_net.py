c9eef1b1b23b109c6555e231900bafbdeef5c99b
wapj <wapj2000@gmail.com>
2024-10-15 18:51:31 +0900

    5강 역전파

--- deep-learning-from-scratch/two_layer_net.py
+++ deep-learning-from-scratch/two_layer_net.py
@@ -1,18 +1,22 @@
-import sys, os
+import os
+import sys
+
 sys.path.append(os.pardir)
-import numpy as np
 
 from common.functions import *
 from common.gradient import numerical_gradient
 
+
 class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
-        self.params = {}
-        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params['b2'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b2'] = np.zeros(output_size)
+        self.params = {
+            "W1": weight_init_std * np.random.randn(input_size, hidden_size),
+            "b1": np.zeros(hidden_size),
+            "W2": weight_init_std * np.random.randn(hidden_size, output_size),
+            "b2": np.zeros(output_size)
+
+        }
 
     def predict(self, x):
         W1, W2 = self.params['W1'], self.params['W2']
@@ -68,8 +72,3 @@ class TwoLayerNet:
         grads['b1'] = np.sum(dz1, axis=0)
 
         return grads
-
-
-
-
-
